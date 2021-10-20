a072b45b3c15b96856cff76f611fa10d59f54c62
Arun S <gnu.arun@gmail.com>
2021-10-20 12:07:02 +0530

    add metrics

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 import asyncio
 from fastapi import FastAPI
 from starlette.responses import StreamingResponse
+from prometheus_fastapi_instrumentator import Instrumentator
 
 
-def smallchunk(smallsize=1000000):
+app = FastAPI()
+Instrumentator().instrument(app).expose(app)
+
+
+def smallchunk():
     '''Generate a long string of a's'''
     res = 'a'*1000*1000
     res += '\n\n'
@@ -14,9 +19,6 @@ async def textgen3(size=3):
     for i in range(size):
         yield smallchunk()
         await asyncio.sleep(0.01)
-
-
-app = FastAPI()
 
 
 @app.get("/size/{filesize}")
