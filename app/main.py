73c38dbee35e23df306cfa0f78f0b554dc9ce69c
Arun S <gnu.arun@gmail.com>
2021-10-20 12:30:44 +0530

    Help text

--- app/main.py
+++ app/main.py
@@ -1,11 +1,22 @@
 import asyncio
 from fastapi import FastAPI
 from starlette.responses import StreamingResponse
+from fastapi.responses import PlainTextResponse
 from prometheus_fastapi_instrumentator import Instrumentator
 
 
 app = FastAPI()
 Instrumentator().instrument(app).expose(app)
+
+
+WELCOME_MESSAGE = '''
+Usage:
+  curl https://filegen.arunsr.in/size/1
+      <- returns a 1mb file
+
+  curl https://filegen.arunsr.in/size/10
+      <- returns a 10mb file
+'''
 
 
 def smallchunk():
@@ -21,8 +32,14 @@ async def textgen3(size=3):
         await asyncio.sleep(0.01)
 
 
+@app.get("/", response_class=PlainTextResponse)
+def home():
+    return WELCOME_MESSAGE
+
+
 @app.get("/size/{filesize}")
 def read_stream(filesize: int):
-    # generator = textgen3(int(filesize)*1000)
+    if filesize > 2000:
+        return("Try a smaller file size, please")
     generator = textgen3(filesize)
     return StreamingResponse(generator, media_type='text/plain')
